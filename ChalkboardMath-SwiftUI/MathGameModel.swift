cfc89226c451b53f70112ac99ce8f2acb3a0b030
Kurt Niemi <kurtn718@gmail.com>
2020-10-16 20:03:58 -0400

    Define model for Chalkboard Math Game

---
+++ ChalkboardMath-SwiftUI/MathGameModel.swift
@@ -0,0 +1,15 @@
+//
+//  MathGameModel.swift
+//  ChalkboardMath-SwiftUI
+//
+//  Created by Kurt Niemi on 10/16/20.
+//
+
+import Foundation
+
+struct MathGameModel {
+    var selectedNumbers: Set<Int> = Set<Int>()
+    var flashcardMode:Bool = false
+    var playChalkman:Bool = false
+}
+
